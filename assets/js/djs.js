cfe7a3b84c9df55f7f50c8fb65540ae26db7b374
tamimi-dev <60556811+tamimi-dev@users.noreply.github.com>
2020-02-06 21:31:18 -0500

    Update djs.js

--- assets/js/djs.js
+++ assets/js/djs.js
@@ -1,12 +1,14 @@
-      function loadData() {
-          var url = "https://docs.google.com/spreadsheet/pub?key=p_aHW5nOrj0VO2ZHTRRtqTQ&single=true&gid=0&range=A1&output=csv";
-          xmlhttp = new XMLHttpRequest();
-          xmlhttp.onreadystatechange = function () {
-              if (xmlhttp.readyState == 4) {
-                  document.getElementById("display").innerHTML = xmlhttp.responseText;
-                  //alert(xmlhttp.responseText);
-              }
-          };
-          xmlhttp.open("GET", url, true);
-          xmlhttp.send(null);
+$.getJSON("https://spreadsheets.google.com/feeds/list/1fgjVhzrbqcCOP8Zls00BV--JsIXEenwWmMD2iF8X9VE/odifzh/public/values?alt=json", function (data) {
+
+      var sheetData = data.feed.entry;
+
+      var i;
+      for (i = 0; i < sheetData.length; i++) {
+
+        var death = data.feed.entry[i]['gsx$death']['$t'];
+
+        document.getElementById('death').innerHTML += ('<tr>'+'<td>'+death+'</td>'+'</tr>');
+
       }
+    });
+
