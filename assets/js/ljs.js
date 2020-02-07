05d3b5170bd9a56aab5a3e7369f5a95988639dd9
tamimi-dev <60556811+tamimi-dev@users.noreply.github.com>
2020-02-07 02:10:34 -0500

    Update ljs.js

--- assets/js/ljs.js
+++ assets/js/ljs.js
@@ -4,13 +4,13 @@ $.getJSON("https://spreadsheets.google.com/feeds/list/1fgjVhzrbqcCOP8Zls00BV--Js
 
       var i;
       for (i = 0; i < sheetData.length; i++) {
-
+	var date = data.feed.entry[i]['gsx$date']['$t'];
         var time = data.feed.entry[i]['gsx$time']['$t'];
         var cose = data.feed.entry[i]['gsx$case']['$t'];
         var loc = data.feed.entry[i]['gsx$location']['$t'];
 		var desc = data.feed.entry[i]['gsx$description']['$t'];
 
-                document.getElementById('timel').innerHTML += ('<article class="timeline__item">'+'<h5 class="title title--h5 timeline__title">'+cose+' '+'<span class="badge badge-pill badge-danger">'+loc+'</span>'+'</h5>'+'<span class="timeline__period">'+time+'</span>'+'<p class="timeline__description">'+desc+'</p>'+'</article>');
+                document.getElementById('timel').innerHTML += ('<article class="timeline__item">'+'<h5 class="title title--h5 timeline__title">'+cose+' '+'<span class="badge badge-pill badge-danger">'+loc+'</span>'+'</h5>'+'<span class="timeline__period">'date'+'+time+'</span>'+'<p class="timeline__description">'+desc+'</p>'+'</article>');
       }
     });
 
