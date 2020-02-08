0796d9a3250f0f0711dca8f76184a6f155d61a0a
tamimi-dev <60556811+tamimi-dev@users.noreply.github.com>
2020-02-07 19:04:20 -0500

    Add files via upload

---
+++ assets/js/scdjs.js
@@ -0,0 +1,22 @@
+$.getJSON("https://spreadsheets.google.com/feeds/list/1fgjVhzrbqcCOP8Zls00BV--JsIXEenwWmMD2iF8X9VE/okczqou/public/values?alt=json", function (data) {
+
+      var sheetData = data.feed.entry;
+
+      var i;
+      for (i = 0; i < sheetData.length; i++) {
+
+        var susp = data.feed.entry[i]['gsx$susp']['$t'];
+		var confir = data.feed.entry[i]['gsx$conf']['$t'];
+		var death = data.feed.entry[i]['gsx$conf']['$t'];
+
+        document.getElementById('susp').innerHTML += ('<tr>'+'<td>'+susp+'</td>'+'</tr>');
+		document.getElementById('confir').innerHTML += ('<tr>'+'<td>'+confir+'</td>'+'</tr>');
+		document.getElementById('death').innerHTML += ('<tr>'+'<td>'+death+'</td>'+'</tr>');
+
+      }
+    });
+
+
+
+
+
