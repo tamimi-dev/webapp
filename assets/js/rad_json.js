58d12e9dc20f9d8ab81818ded95c2fba46aa7320
tamimi-dev <60556811+tamimi-dev@users.noreply.github.com>
2020-02-12 04:02:55 -0500

    Update rad_json.js

--- assets/js/rad_json.js
+++ assets/js/rad_json.js
@@ -1,82 +1,28 @@
-$.getJSON("https://spreadsheets.google.com/feeds/list/1fgjVhzrbqcCOP8Zls00BV--JsIXEenwWmMD2iF8X9VE/ou32gi9/public/values?alt=json", function (data) {
-
-      var sheetData = data.feed.entry;
-
-      var i;
-      for (i = 0; i < sheetData.length; i++) {
-
-        var cnt = data.feed.entry[i]['gsx$countries']['$t'];
-		var cse = data.feed.entry[i]['gsx$totcasepercntry']['$t'];
-
-
-var randomScalingFactor = function() {
-  return Math.round(Math.random() * 100);
-};
-var chartColors = {
-	red: 'rgba(253, 48, 76)',
-	orange: 'rgb(255, 159, 64)',
-	yellow: 'rgba(240,236,211)',
-	green: 'rgb(75, 192, 192)',
-	blue: 'rgba(42,105,163)',
-	purple: 'rgb(153, 102, 255)',
-	grey: 'rgba(48,48,50)'
-};
-
-var color = Chart.helpers.color;
-var config = {
-  type: 'radar',
-  data: {
-    labels: cnt,
-    datasets: [{
-      label: "Current level",
-      backgroundColor: color(chartColors.red).alpha(0.2).rgbString(),
-      borderColor: chartColors.red,
-      pointBackgroundColor: chartColors.red,
-      data: cse
-    },]
-  },
-  options: {
-    legend: {
-      position: 'top',
-      labels: {
-        fontColor: 'white'
-      }
+$.getJSON("https://spreadsheets.google.com/feeds/list/1GnakUnNQvFXjuzMSPnBpU9eufb4SooQLGL2oFc3lfAs/od6/public/values?alt=json", data => {
+  var labels = [];
+  var numbers = [];
+  data.feed.entry.forEach(e => {
+    labels.push(e['gsx$names']['$t']);
+    numbers.push(Number(e['gsx$numbers']['$t']));
+  });
+  new Chart(document.getElementById('canvas'), {
+    type: 'radar',
+    data: {
+      labels: labels,
+      datasets: [{
+        label: 'Current level',
+        data: numbers,
+        backgroundColor: 'rgba(253, 48, 76, 0.2)',
+        borderColor: 'rgb(253, 48, 76)',
+        pointBackgroundColor: 'rgb(253, 48, 76)'
+      }]
     },
-    title: {
-      display: true,
-      text: 'Curent level Skills vs desired',
-      fontColor: 'white'
-    },
-	  maintainAspectRatio: false,
-    scale: {
-      ticks: {
-        beginAtZero: true,
-        fontColor: 'white', // labels such as 10, 20, etc
-        showLabelBackdrop: false // hide square behind text
-      },
-      pointLabels: {
-        fontColor: 'white' // labels around the edge like 'Running'
-      },
-      gridLines: {
-        color: 'rgba(255, 255, 255, 0.2)'
-      },
-      angleLines: {
-        color: 'white' // lines radiating from the center
+    options: {
+      tooltips: {
+        callbacks: {
+          title: (tooltipItem, data) => data.labels[tooltipItem[0].index]
+        }
       }
     }
-  }
-};
-
-// A plugin to draw the background color
-Chart.plugins.register({
-  beforeDraw: function(chartInstance) {
-    var ctx = chartInstance.chart.ctx;
-    ctx.fillStyle = '#303032';
-    ctx.fillRect(0, 0, chartInstance.chart.width, chartInstance.chart.height);
-  }
-})
-
-window.myRadar = new Chart(document.getElementById("canvas"), config);
-
-      }
-    });
+  });
+});
