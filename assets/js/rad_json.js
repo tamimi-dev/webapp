c28e0c5bfcc48e9f3606111e759bda7f8acc131e
tamimi-dev <60556811+tamimi-dev@users.noreply.github.com>
2020-02-12 00:36:55 -0500

    Update rad_json.js

--- assets/js/rad_json.js
+++ assets/js/rad_json.js
@@ -1,56 +1,71 @@
+function BuildChart(labels, values, chartTitle) {
+    var data = {
+        labels: labels,
+        datasets: [{
+            label: chartTitle, // Name the series
+            data: values,
+            backgroundColor: ['rgb(54, 162, 235)',
+                'rgb(54, 162, 235)',
+                'rgb(54, 162, 235)',
+                'rgb(54, 162, 235)',
+                'rgb(54, 162, 235)',
+                'rgb(54, 162, 235)',
+                'rgb(54, 162, 235)',
+                'rgb(54, 162, 235)',
+                'rgb(54, 162, 235)',
+                'rgb(54, 162, 235)',
+            ],
+        }],
+    };
 
-$(function(){var sheetUrl = 'https://spreadsheets.google.com/feeds/list/1fgjVhzrbqcCOP8Zls00BV--JsIXEenwWmMD2iF8X9VE/ou32gi9/public/values?alt=json';$.getJSON(sheetUrl, function(data){
-	var sheetData = data.feed.entry;
-	var i;
-      for (i = 0; i < sheetData.length; i++) {
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'horizontalBar',
+        data: data,
+        options: {
+            responsive: true, // Instruct chart js to respond nicely.
+            maintainAspectRatio: false, // Add to prevent default behaviour of full-width/height 
+            scales: {
+                xAxes: [{
+                        scaleLabel: {
+                        display: true,
+                        labelString: '$ Billion'
+                    }
+                }],
+                yAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Name'
+                    }
+                }]
+            },
+        }
+    });
 
-        var cntry = data.feed.entry[i]['gsx$countries']['$t'];
-		var cntry_case = data.feed.entry[i]['gsx$totcasepercntry']['$t'];
-  }
-  
-  console.log(cntry);
-  console.log(cntry_case);
-})
+    return myChart;
+}
+
+
+
+// Ref - https://github.com/jesseokeya/Forbes400  / https://forbes400.herokuapp.com/
+
+var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var json = JSON.parse(this.response);
+      
+// Map json labels  back to values array
+var labels = json.map(function (e) {
+    return e.person.name;
+});
+      
+// Map json values back to values array
+var values = json.map(function (e) {
+    return (e.finalWorth); // Divide to billions in units of ten
 });
 
-
-var ctx = document.getElementById("canvas").getContext('2d');
-var canvas = new Chart(ctx, {
-          type: 'line',
-          data: {
-              datasets: [{
-                  label: 'Countries',
-                  fill: false,
-                  data: cntry,
-                  borderColor: '#F3A68E',
-                  borderWidth: 3,
-                  backgroundColor: '#F3A68E',
-                  borderDash: [10,5],
-              },{
-                label: 'Total Cases',
-                fill: false,
-                data: cntry_case,
-                borderColor: '#698C01',
-                borderWidth: 3,
-                backgroundColor: '#698C01',
-                borderDash: [10,5],
-              }
-            ]
-          },
-          options: {
-              scales: {
-                  yAxes: [{
-                      ticks: {
-                          beginAtZero:true
-                      }
-                  }]
-              },
-              legend: {
-                position: 'bottom',
-                labels:{
-                  boxWidth: 15,
-                  fontSize: 15,
-                },
-              },
-          }
-      });
+BuildChart(labels, values, "Real Time Net Worth");
+    }
+  };
+  xhttp.open("GET", "https://spreadsheets.google.com/feeds/list/1fgjVhzrbqcCOP8Zls00BV--JsIXEenwWmMD2iF8X9VE/ou32gi9/public/values?alt=json", false);
+  xhttp.send();
