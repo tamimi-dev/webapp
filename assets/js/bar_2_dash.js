a8f6cdcfc149ea6125b546f20b12c90988c5977b
tamimi-dev <60556811+tamimi-dev@users.noreply.github.com>
2020-02-12 20:45:04 -0500

    Create bar_2_dash.js

---
+++ assets/js/bar_2_dash.js
@@ -0,0 +1,41 @@
+$.getJSON("https://spreadsheets.google.com/feeds/list/1fgjVhzrbqcCOP8Zls00BV--JsIXEenwWmMD2iF8X9VE/ou32gi9/public/values?alt=json", data => {
+  var labels = [];
+  var numbers = [];
+  var numbers2 = [];
+  data.feed.entry.forEach(e => {
+    labels.push(e['gsx$timecase']['$t']);
+    numbers.push(Number(e['gsx$totcaseall']['$t']));
+	numbers2.push(Number(e['gsx$dincreaseall']['$t']));
+  });
+  new Chart(document.getElementById('canvas'), {
+    type: 'horizontalBar',
+    data: {
+      labels: labels,
+      datasets: [{
+        label: 'Daily Increase',
+        data: numbers,
+        backgroundColor: 'rgba(42,105,163)',
+        borderColor: 'rgba(42,105,163)',
+        pointBackgroundColor: 'rgba(240,236,211)'
+      },
+	  {
+      labels: labels,
+      datasets: [{
+        label: 'Acc. Daily Total',
+        data: numbers2,
+        backgroundColor: 'rgb(253, 48, 76)',
+        borderColor: 'rgb(253, 48, 76)',
+        pointBackgroundColor: 'rgba(240,236,211)'
+      }
+	  ]
+    },
+    options: {
+      	  maintainAspectRatio: false,
+      tooltips: {
+        callbacks: {
+          title: (tooltipItem, data) => data.labels[tooltipItem[0].index]
+        }
+      }
+    }
+  });
+});
