e1933469697972fb56232f6a40a8596b618caca2
tamimi-dev <60556811+tamimi-dev@users.noreply.github.com>
2020-02-12 22:52:51 -0500

    Update bar_2_dash.js

--- assets/js/bar_2_dash.js
+++ assets/js/bar_2_dash.js
@@ -2,10 +2,12 @@ $.getJSON("https://spreadsheets.google.com/feeds/list/1fgjVhzrbqcCOP8Zls00BV--Js
   var labels = [];
   var numbers = [];
    var numbers2 = [];
+	var numbers3 = [];
   data.feed.entry.forEach(e => {
     labels.push(e['gsx$timecase']['$t']);
     numbers.push(Number(e['gsx$totcaseall']['$t']));
 	numbers2.push(Number(e['gsx$dincreaseall']['$t']));
+	  numbers3.push(Number(e['gsx$percinc']['$t']));
   });
   new Chart(document.getElementById('canvas'), {
     type: 'horizontalBar',
@@ -24,7 +26,15 @@ $.getJSON("https://spreadsheets.google.com/feeds/list/1fgjVhzrbqcCOP8Zls00BV--Js
         backgroundColor: 'rgb(255, 159, 64)',
         borderColor: 'rgb(255, 159, 64)',
         pointBackgroundColor: 'rgb(240,236,211)'
-      }
+      },
+	{
+        label: 'Daily % Change',
+        data: numbers3,
+        backgroundColor: 'rgb(75, 192, 192)',
+        borderColor: 'rgb(75, 192, 192)',
+        pointBackgroundColor: 'rgb(240,236,211)'
+	type: 'line'
+      } 
 	  ]
     },
     options: {
