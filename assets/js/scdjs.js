1c6d3004ff953cc096ae7a7a26fd87cb85deceff
tamimi-dev <60556811+tamimi-dev@users.noreply.github.com>
2020-02-13 23:48:16 -0500

    Update scdjs.js

--- assets/js/scdjs.js
+++ assets/js/scdjs.js
@@ -7,15 +7,11 @@ $.getJSON("https://spreadsheets.google.com/feeds/list/1fgjVhzrbqcCOP8Zls00BV--Js
 
         var susp = data.feed.entry[i]['gsx$susp']['$t'];
 		var confir = data.feed.entry[i]['gsx$conf']['$t'];
-		var death = data.feed.entry[i]['gsx$death']['$t'];
-	      var recov = data.feed.entry[i]['gsx$recover']['$t'];
-	      var seri = data.feed.entry[i]['gsx$serious']['$t'];
+
 
         document.getElementById('susp').innerHTML += ('<tr>'+'<td>'+susp+'</td>'+'</tr>');
 		document.getElementById('confir').innerHTML += ('<tr>'+'<td>'+confir+'</td>'+'</tr>');
-		document.getElementById('death').innerHTML += ('<tr>'+'<td>'+death+'</td>'+'</tr>');
-	      	document.getElementById('recov').innerHTML += ('<tr>'+'<td>'+recov+'</td>'+'</tr>');
-	      	document.getElementById('seri').innerHTML += ('<tr>'+'<td>'+seri+'</td>'+'</tr>');
+
 
       }
     });
