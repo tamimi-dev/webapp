ad3539ff80da6a95ce376601e35ec721e5739ef2
tamimi-dev <60556811+tamimi-dev@users.noreply.github.com>
2020-02-18 01:04:18 -0500

    Create mjs.js

---
+++ assets/js/mjs.js
@@ -0,0 +1,23 @@
+$.getJSON("https://spreadsheets.google.com/feeds/list/1fgjVhzrbqcCOP8Zls00BV--JsIXEenwWmMD2iF8X9VE/o3brp63/public/values?alt=json", function (data) {
+
+      var sheetData = data.feed.entry;
+
+      var i;
+      for (i = 0; i < sheetData.length; i++) {
+
+        var score = data.feed.entry[i]['gsx$score']['$t'];
+        var crit = data.feed.entry[i]['gsx$crit']['$t'];
+        var critb = data.feed.entry[i]['gsx$critb']['$t'];
+        var one = data.feed.entry[i]['gsx$one']['$t'];
+        var two = data.feed.entry[i]['gsx$two']['$t'];
+        var three = data.feed.entry[i]['gsx$three']['$t'];
+        var four = data.feed.entry[i]['gsx$four']['$t'];       
+
+
+        document.getElementById('score').innerHTML += ('<tr><th scope="row">'+crit+'</th><td>'+one+'</td><td>'+two+'</td><td>'+three+'</td><td>'+four+'</td><td class="bg-danger">'+score+'</td></tr>');
+
+      }
+    });
+    
+    
+    
