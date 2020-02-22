d20f26cfd1c1b89350d2890d39a13dad116001bb
tamimi-dev <60556811+tamimi-dev@users.noreply.github.com>
2020-02-22 04:36:24 -0500

    Update rotating.js

--- assets/js/rotating.js
+++ assets/js/rotating.js
@@ -1,11 +1,6 @@
-
-
-// Themes begin
 am4core.useTheme(am4themes_material);
 am4core.useTheme(am4themes_animated);
 // Themes end
-
-
 
 // Create map instance
 var chart = am4core.create("chartdiv", am4maps.MapChart);
@@ -17,7 +12,7 @@ chart.geodata = am4geodata_worldLow;
 chart.projection = new am4maps.projections.Orthographic();
 chart.panBehavior = "rotateLongLat";
 chart.deltaLatitude = -20;
-chart.padding(20,20,20,20);
+chart.padding(20, 20, 20, 20);
 
 chart.backgroundSeries.mapPolygons.template.polygon.fillOpacity = 0.1;
 chart.backgroundSeries.mapPolygons.template.polygon.fill = am4core.color("#ffffff");
@@ -26,11 +21,11 @@ chart.deltaLongitude = 20;
 chart.deltaLatitude = -20;
 
 // limits vertical rotation
-chart.adapter.add("deltaLatitude", function(delatLatitude){
-    return am4core.math.fitToRange(delatLatitude, -90, 90);
+chart.adapter.add("deltaLatitude", function(delatLatitude) {
+  return am4core.math.fitToRange(delatLatitude, -90, 90);
 })
 
-// Create map polygon series
+
 
 
 
@@ -53,23 +48,23 @@ template.propertyFields.id = "id";
 template.tooltipPosition = "fixed";
 template.fillOpacity = 1;
 
-template.events.on("over", function (event) {
+template.events.on("over", function(event) {
   if (event.target.dummyData) {
     event.target.dummyData.isHover = true;
   }
 })
-template.events.on("out", function (event) {
+template.events.on("out", function(event) {
   if (event.target.dummyData) {
     event.target.dummyData.isHover = false;
   }
 })
- 
+
 
 
 
 var hs = polygonSeries.mapPolygons.template.states.create("hover");
 hs.properties.fillOpacity = 1;
-hs.properties.fill = chart.colors.getIndex(12).brighten(0.5);
+hs.properties.fill = chart.colors.getIndex(0).brighten(-0.5);
 
 
 var graticuleSeries = chart.series.push(new am4maps.GraticuleSeries());
@@ -87,51 +82,54 @@ measelsSeries.tooltip.label.fill = am4core.color("#000");
 measelsSeries.tooltip.dy = -5;
 
 var measelTemplate = measelsSeries.mapPolygons.template;
-measelTemplate.fill = am4core.color("#fff");
+measelTemplate.fill = am4core.color("#f9e3ce");
 measelTemplate.strokeOpacity = 0;
 measelTemplate.fillOpacity = 0.75;
 measelTemplate.tooltipPosition = "fixed";
 
 let animation;
-setTimeout(function(){
-  animation = chart.animate({property:"deltaLongitude", to:100000}, 20000000);
+setTimeout(function() {
+  animation = chart.animate({
+    property: "deltaLongitude",
+    to: 100000
+  }, 20000000);
 }, 3000)
 
-chart.seriesContainer.events.on("down", function(){
-if(animation){
-  animation.stop();
-}
+chart.seriesContainer.events.on("down", function() {
+  if (animation) {
+    animation.stop();
+  }
 })
 
-
-
-polygonSeries.events.on("inited", function () {
-  polygonSeries.mapPolygons.each(function (mapPolygon) {
-    var count = data[mapPolygon.id];
+const handleMapData = (mapData) => {
+  polygonSeries.mapPolygons.each(function(mapPolygon) {
+    var count = mapData[mapPolygon.id];
 
     if (count > 0) {
       var polygon = measelsSeries.mapPolygons.create();
       polygon.multiPolygon = am4maps.getCircle(mapPolygon.visualLongitude, mapPolygon.visualLatitude, Math.max(0.2, Math.log(count) * Math.LN10 / 10));
       polygon.tooltipText = mapPolygon.dataItem.dataContext.name + ": " + count;
       mapPolygon.dummyData = polygon;
-      polygon.events.on("over", function () {
+      polygon.events.on("over", function() {
         mapPolygon.isHover = true;
       })
-      polygon.events.on("out", function () {
+      polygon.events.on("out", function() {
         mapPolygon.isHover = false;
       })
-    }
-    else {
+    } else {
       mapPolygon.tooltipText = mapPolygon.dataItem.dataContext.name + ": no data";
       mapPolygon.fillOpacity = 0.9;
-      mapPolygon.fill = am4core.color("#47c769");
+      mapPolygon.fill = am4core.color("#915660");
     }
 
   })
-})
-
-
-var data = {
-  "CN": 68502,
-  "JP": 100
 }
+
+fetch("https://spreadsheets.google.com/feeds/list/1fgjVhzrbqcCOP8Zls00BV--JsIXEenwWmMD2iF8X9VE/o512ru7/public/values?alt=json").then(response => response.json()).then((jsonData) => {
+  const mapData = {}
+
+  jsonData.feed.entry.forEach(e => {
+    mapData[e['gsx$icon']['$t']] = Number(e['gsx$icontotcasepercntry']['$t'])
+  })
+  handleMapData(mapData)
+})
