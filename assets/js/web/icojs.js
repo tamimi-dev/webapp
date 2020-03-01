dfaaad28be0c1ea77e9f4c84bee143c383ce1c18
tamimi-dev <60556811+tamimi-dev@users.noreply.github.com>
2020-03-01 04:49:25 -0500

    Update icojs.js

--- assets/js/web/icojs.js
+++ assets/js/web/icojs.js
@@ -10,6 +10,6 @@ $.getJSON("https://spreadsheets.google.com/feeds/list/1fgjVhzrbqcCOP8Zls00BV--Js
         var ico = data.feed.entry[i]['gsx$icon']['$t'];
         var dth = data.feed.entry[i]['gsx$dthcntry']['$t'];
 
-        document.getElementById('ico_cntry').innerHTML += ('<div class="col-lg-4 col-xl-4">'+'<div class="box box__second ">'+'<svg class="avatar avatar--120">'+'<g class="avatar__box">'+'<image xlink:href="https://tamimi-dev.github.io/app/assets/icons/cntry/'+ico+'.svg"'+' height="100%"'+'width="100%"></image></g></svg><div class="text-center"><h4 class="title title--h4 sidebar__user-name">'+cntry+'</h4></div><div class="text-center"><table class="table5" align="center"><tr><th>Cases</th><th>Deaths</th></tr><tr><td>'+cntry_case+'</td><td>'+dth+'</td></tr></table></div></div></div>');
+        document.getElementById('ico_cntry').innerHTML += ('<div class="col-lg-4 col-xl-4">'+'<div class="box box__second ">'+'<svg class="avatar avatar--120">'+'<g class="avatar__box">'+'<image xlink:href="../assets/icons/cntry/'+ico+'.svg"'+' height="100%"'+'width="100%"></image></g></svg><div class="text-center"><h4 class="title title--h4 sidebar__user-name">'+cntry+'</h4></div><div class="text-center"><table class="table5" align="center"><tr><th>Cases</th><th>Deaths</th></tr><tr><td>'+cntry_case+'</td><td>'+dth+'</td></tr></table></div></div></div>');
       }
     });
