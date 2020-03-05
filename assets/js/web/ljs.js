89a0ebd75cce39722dd36e98041767539d0f1c2b
tamimi-dev <60556811+tamimi-dev@users.noreply.github.com>
2020-03-05 02:52:50 -0500

    Update ljs.js

--- assets/js/web/ljs.js
+++ assets/js/web/ljs.js
@@ -1,4 +1,4 @@
-$.getJSON("https://spreadsheets.google.com/feeds/list/1fgjVhzrbqcCOP8Zls00BV--JsIXEenwWmMD2iF8X9VE/o7om10o/public/values?alt=json", function (data) {
+$.getJSON("https://spreadsheets.google.com/feeds/list/12Lyd3k0PRpe-Ie_LaeJaEPm3FwF3iz1gizAOSprn7l0/o7om10o/public/values?alt=json", function (data) {
 
       var sheetData = data.feed.entry;
 
